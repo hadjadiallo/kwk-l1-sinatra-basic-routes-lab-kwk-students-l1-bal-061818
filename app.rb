4ea60b5d3022f64793fbfc51f58dbcf81ed3ad43
hadjadiallo <hadjadiallo0101@gmail.com>
2018-06-29 14:54:33 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
-
+ 
 class App < Sinatra::Base
+  get '/'do 
+     "my name is hadja"
+  end 
+  
+  get '/'do
+   "my hometown is idk' do"
+  end 
+  
+  get'/'do 
+    "my favorite song is_"
+  end
 end
